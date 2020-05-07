defabfe0e60d30f1d321a809f5e56df4dd98d37e
Saratu Muazu <sarahembee71@gmail.com>
2020-05-06 22:19:42 -0700

    ..

--- Database/db.js
+++
@@ -1,8 +0,0 @@
-// var MongoClient = require('mongodb').MongoClient;
-// var url = "mongodb://localhost:27017/Sarah";
-
-// MongoClient.connect(url, function(err, db) {
-//     if (err) throw err;
-//     console.log("Database Created by Sarah");
-//     db.close();
-// });
